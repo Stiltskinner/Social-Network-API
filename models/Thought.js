b00505691727566432719048d7d5cec10650eb06
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-05-21 16:45:55 -0500

    Created Thought model

---
+++ models/Thought.js
@@ -0,0 +1,35 @@
+const { Schema, Types } = require('mongoose');
+const Reaction = require('./Reaction');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+          type: String,
+          required: true,
+          maxlength: 280,
+          minlength: 1
+        },
+        createdAt: {
+          type: Date,
+          default: new Date().toString(),
+        },
+        username: {
+            type: String,
+            required: true
+        }  ,
+        reactions: [Reaction]     
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        }
+    }
+);
+
+thoughtSchema.virtual('reactionCount')
+.get(function () {
+    return `Number of reaction: ${this.reactions.length}`;
+})
+
+// How to use a getter method to format the timestamp on query? What does getters: true actually do?
